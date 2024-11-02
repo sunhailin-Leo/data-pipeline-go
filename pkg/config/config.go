40b93cb392db532a571010e35d8f44448dd1762c
sunhailinLeo <379978424@qq.com>
2024-11-02 20:55:33 +0800

    update go.mod; update config.go and loaders.go;

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,8 +5,8 @@ var TunnelCfg *TunnelConfig
 
 // SourceConfig Source config
 type SourceConfig struct {
-	Type        string                  `json:"type"`
-	SourceName  string                  `json:"source_name"`
+	Type        string                  `json:"type" vd:"len($)>0"`
+	SourceName  string                  `json:"source_name" vd:"len($)>0"`
 	Kafka       KafkaSourceConfig       `json:"kafka"`
 	RocketMQ    RocketMQSourceConfig    `json:"rocketmq"`
 	RabbitMQ    RabbitMQSourceConfig    `json:"rabbitmq"`
@@ -17,16 +17,16 @@ type SourceConfig struct {
 
 // TransformSchema transform unit config
 type TransformSchema struct {
-	SourceKey    string `json:"source_key"`
-	SinkKey      string `json:"sink_key"`
-	Converter    string `json:"converter"`      // Converter, Like: toInt, toFloat32, toString, etc.
-	IsIgnore     bool   `json:"is_ignore"`      // is ignored key
-	IsStrictMode bool   `json:"is_strict_mode"` // is strict mode
-	IsKeepKeys   bool   `json:"is_keep_keys"`   // key is keep origin key
-	IsExpand     bool   `json:"is_expand"`      // is expanded col
-	ExpandValue  any    `json:"expand_value"`   // expand value
-	SourceName   string `json:"source_name"`
-	SinkName     string `json:"sink_name"`
+	SourceKey    string `json:"source_key" vd:"len($)>0"`  // source key
+	SinkKey      string `json:"sink_key" vd:"len($)>0"`    // sink key
+	Converter    string `json:"converter"`                 // Converter, Like: toInt, toFloat32, toString, etc.
+	IsIgnore     bool   `json:"is_ignore"`                 // is ignored key
+	IsStrictMode bool   `json:"is_strict_mode"`            // is strict mode
+	IsKeepKeys   bool   `json:"is_keep_keys"`              // key is keep origin key
+	IsExpand     bool   `json:"is_expand"`                 // is expanded col
+	ExpandValue  any    `json:"expand_value"`              // expand value
+	SourceName   string `json:"source_name" vd:"len($)>0"` // source alias name
+	SinkName     string `json:"sink_name" vd:"len($)>0"`   // sink alias name
 }
 
 // TransformJsonPath transform json path config
@@ -38,16 +38,16 @@ type TransformJsonPath struct {
 
 // TransformConfig transform config
 type TransformConfig struct {
-	Mode         string              `json:"mode"`          // Mode: row, json
-	Schemas      []TransformSchema   `json:"schemas"`       // Schema
-	RowSeparator string              `json:"row_separator"` // only mode is row will affect, and also only row mode will use strings.Split to split
-	Paths        []TransformJsonPath `json:"paths"`         // json paths
+	Mode         string              `json:"mode" vd:"len($)>0"` // Mode: row, json
+	Schemas      []TransformSchema   `json:"schemas"`            // Schema
+	RowSeparator string              `json:"row_separator"`      // only mode is row will affect, and also only row mode will use strings.Split to split
+	Paths        []TransformJsonPath `json:"paths"`              // json paths
 }
 
 // SinkConfig Sink config
 type SinkConfig struct {
-	Type          string                  `json:"type"`
-	SinkName      string                  `json:"sink_name"`
+	Type          string                  `json:"type" vd:"len($)>0"`
+	SinkName      string                  `json:"sink_name" vd:"len($)>0"`
 	Clickhouse    ClickhouseSinkConfig    `json:"clickhouse"`
 	HTTP          HTTPSinkConfig          `json:"http"`
 	Kafka         KafkaSinkConfig         `json:"kafka"`
@@ -64,13 +64,13 @@ type SinkConfig struct {
 
 // StreamConfig stream config
 type StreamConfig struct {
-	Name          string          `json:"name"`         // stream name
-	Enable        bool            `json:"enable"`       // is enabled stream
-	ChannelSize   int             `json:"channel_size"` // 通道缓冲区大小（用于削峰，不给默认 0）
-	Source        []*SourceConfig `json:"source"`       // 数据源配置项
-	SourceAckMode int             `json:"ack_mode"`     // 仅当 Source 时 MQ 组件才有效; 0: 消费后提交; 1: 转换成功后提交; 2: 发送后提交
-	Transform     TransformConfig `json:"transform"`    // 数据转换配置项
-	Sink          []*SinkConfig   `json:"sink"`         // 数据目标配置项
+	Name          string          `json:"name" vd:"len($)>0"`   // stream name
+	Enable        bool            `json:"enable"`               // is enabled stream
+	ChannelSize   int             `json:"channel_size"`         // channel buffer size（default: 0）
+	Source        []*SourceConfig `json:"source" vd:"len($)>0"` // source config slice
+	SourceAckMode int             `json:"ack_mode"`             // It worked only Source is MQ component; 0: commit after consume; 1: commit after transform; 2: commit after sink
+	Transform     TransformConfig `json:"transform"`            // transform config
+	Sink          []*SinkConfig   `json:"sink" vd:"len($)>0"`   // sink config slice
 }
 
 // GetSourceBySourceName based on SourceName to get Source
@@ -115,5 +115,5 @@ func (s *StreamConfig) GetSinkTagBySinkName(name string) string {
 
 // TunnelConfig data tunnel config struct.
 type TunnelConfig struct {
-	Streams []*StreamConfig `json:"streams"`
+	Streams []*StreamConfig `json:"streams" vd:"len($)>0"`
 }
